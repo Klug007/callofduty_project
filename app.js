d77bf1d59c46b8b10aa80c8e598986a9d50f927e
Tillabayev Yosinbek <121226001+Klug007@users.noreply.github.com>
2024-05-04 12:14:40 +0500

    Minor Bug

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-function checklogin() {
-    let localstorage = localStorage.getItem("key");
-    if (localstorage) {
-        console.log("hammasi joyida");
-    } else {
-        window.location.pathname = "/login";
-    }
-}
+// function checklogin() {
+//     let localstorage = localStorage.getItem("key");
+//     if (localstorage) {
+//         console.log("hammasi joyida");
+//     } else {
+//         window.location.pathname = "/login";
+//     }
+// }
 
-checklogin();
+// checklogin();
 
-function logout() {
-    localStorage.setItem("key", "");
-    window.location.pathname = "/";
-}
+// function logout() {
+//     localStorage.setItem("key", "");
+//     window.location.pathname = "/";
+// }
